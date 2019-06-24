982fe6314312d0217008c86e5d2a4c1198bdaf99
davemos <david.moschkau@gmail.com>
2019-06-24 12:07:50 -0500

    remved unnecessary reerences

--- Target/myRetail/Controllers/ProductsController.cs
+++ Target/myRetail/Controllers/ProductsController.cs
@@ -1,15 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using myRetail.Models;
-using myRetail.Properties;
-using Microsoft.Ajax.Utilities;
-using MongoDB.Bson.IO;
-using MongoDB.Driver;
-using Newtonsoft.Json;
 
 namespace myRetail.Controllers
 {
